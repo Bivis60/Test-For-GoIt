6974570592ebcbfc45e6872f8a40a9216c17e66d
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-09-06 00:43:17 +0300

    Додав загальні стилі

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { GlobalStyle } from 'GlobalStyle';
+import { Route, Routes } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route/>
+      </Routes>
+    </>
+  );
+};
